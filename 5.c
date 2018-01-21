2390fac4a59797d4c8831f37dcfe71aee3650a4b
gde127 <35266992+gde127@users.noreply.github.com>
2018-01-21 05:00:31 +0000

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,12 @@
+#include<stdio.H>
+void main()
+{
+    int n;
+    printf("enter the number");
+    scanf("%d",&n);
+    for(i=1;i<=n;++i)
+    {
+        prinf("the muitilples are",5*i);
+        getch();
+    }
+}
