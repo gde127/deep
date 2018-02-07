6c2fa9ee0b04a51c33dfb440752d322756bf0f9c
gde127 <35266992+gde127@users.noreply.github.com>
2018-02-07 15:51:57 +0530

    Create sums.c

---
+++ sums.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ 
+int main() {
+   int m, n, sum;
+ 
+   printf("\nEnter two no: ");
+   scanf("%d %d", &m, &n);
+ 
+   sum = m+ n;
+ 
+   printf("Sum : %d", sum);
+ 
+   return(0);
