8561f3d1bc9830dab6bcbea82205d6a714fd55f8
gde127 <35266992+gde127@users.noreply.github.com>
2018-02-07 15:46:36 +0530

    Create po2.c

---
+++ po2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+ {
+   int a,b;
+   clrscr();
+   printf("\t Enter the to check whether it is power of 2: ");
+   scanf("%d",&n);
+   a=(b&(b-1));
+   if(m==0)
+       printf("\n\t %d is power of 2",n);
+   else
+       printf("\n\t %d not power of 2",n);
+   getch();
+ }
