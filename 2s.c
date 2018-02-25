cf979b0d6d565ea13c1a9b9d2ec146adf72cc94a
gde127 <35266992+gde127@users.noreply.github.com>
2018-02-25 16:37:47 +0530

    Create 2s.c

---
+++ 2s.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int m,n,o;
+printf("enter the number");
+scanf("%d",&m);
+printf("enter the number");
+scanf("%d',&n);
+o=m*n;
+if(o%2==0)
+printf("even number");
+else
+printf("odd numbner");
+}
