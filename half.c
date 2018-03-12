88054e2467519ea3eb84a5b1eeb94e095312a340
gde127 <35266992+gde127@users.noreply.github.com>
2018-03-12 16:29:01 +0530

    Create half.c

---
+++ half.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+ int i;
+ char a[20];
+ int n;
+ printf("enter the string");
+ scanf("%c",&a[i]);
+ printf("enter the value");
+ scanf("%d",&n);
+ for(i=0;i<n;i++)
+ {
+     printf("\n%c",a[i]);
+     return 0;
+ }
+}
