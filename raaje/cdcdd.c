5b1a78b86033c74c9518f47d98f21bc5ca2f7889
gde127 <35266992+gde127@users.noreply.github.com>
2018-03-16 16:34:30 +0530

    Create cdcdd.c

---
+++ raaje/cdcdd.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,m=0,n,i;
+    printf("enter then number");
+    scanf("%d",&a);
+    for(i=2;i<n/2;i++)
+    {
+        if(a%i==0)
+        {
+            m=1;
+        }
+        
+        
+        if(m==0)
+        {
+            printf("prime number");
+            
+        }
+        else
+        {
+            printf("not a prime number");
+        }
+        }return 0;
+    }
+    
+
